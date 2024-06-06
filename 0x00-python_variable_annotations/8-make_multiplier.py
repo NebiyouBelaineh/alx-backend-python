d229ea8d2b3fee96a6fa75d1c97746524fc270b1
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-06 21:00:57 +0300

    Add solution for task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing a type-annotated function make_multiplier."""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier."""
+    return lambda x: x * multiplier
