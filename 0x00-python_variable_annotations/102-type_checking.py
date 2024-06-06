d3e0dc065754cb71cc6cbe486633025b4c5f0bd0
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-06 22:59:04 +0300

    Fix Docstring

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Returns a list of items/pixels that are zoomed in"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
