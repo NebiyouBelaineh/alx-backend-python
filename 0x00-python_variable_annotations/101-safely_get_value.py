65e50894affc3b29b4d495576f75083141d50085
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-06 22:44:13 +0300

    Fix Docstring

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -9,6 +9,7 @@ T = TypeVar('T')
 def safely_get_value(
                     dct: Mapping, key: Any,
                     default: Union[T, None] = None) -> Union[Any, T]:
+    """Returns a value from a dictionary safely."""
     if key in dct:
         return dct[key]
     else:
