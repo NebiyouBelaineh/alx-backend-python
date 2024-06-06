1b6ab34f1f8d9666ccbc34cd7e2f8b842137da63
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-06 20:26:27 +0300

    Add solution for task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing a function that returns a sum of a list of float numbers
+."""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns the sum of a list of floats."""
+    return sum(input_list)
