3839c036c61bff2e963174c21148e1101794d41f
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-06 21:27:59 +0300

    Fix docstring

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,4 +5,5 @@ from typing import Tuple, List, Sequence, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples containing the index and the length"""
     return [(i, len(i)) for i in lst]
