52aad6ee0a6ec6da0122512a6e4e1e4a4131c515
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-06 20:10:48 +0300

    Add solution for task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module containing a function that returns the floor of a float."""
+from math import floor as flr
+
+
+def floor(number: float) -> int:
+    return flr(number)
