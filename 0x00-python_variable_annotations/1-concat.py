668a204149dc983c7d50cd5055be3e29392c44b9
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-06 20:06:37 +0300

    Add solution for task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module containing a funcion that adds two floating-point numbers."""
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings.
+
+    Args:
+        str1 (str): The first string to concatenate.
+        str2 (str): The second string to concatenate.
+
+    Returns:
+        str: The concatenation of str1 and str2.
+    """
+    return str1 + str2
