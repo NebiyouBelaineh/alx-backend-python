c1592626219f4cfd6a12704bc50e68f56c47d0da
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-06 20:04:29 +0300

    Change shebang

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,15 @@
-#!/usr/bin/python3
-"""Module for add function"""
+#!/usr/bin/env python3
+"""Module containing a funcion that adds two floating-point numbers."""
 
 
 def add(a: float, b: float) -> float:
+    """Adds two floating-point numbers.
+
+    Args:
+        a (float): The first number to add.
+        b (float): The second number to add.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
