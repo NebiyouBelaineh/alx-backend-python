164eddd2aa694c23fa433bc5677656c1073837f4
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-11 00:20:27 +0300

    Add solution for task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+import asyncio
+from typing import List
+"""Module for printing the correct output of the wait_n coroutine"""
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
