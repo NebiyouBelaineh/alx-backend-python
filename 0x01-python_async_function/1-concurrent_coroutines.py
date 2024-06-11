2c68f029b53abf976f4e35c82b7e15dabe1b6450
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-11 16:09:50 +0300

    Add docstring and change approach

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,10 +9,18 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Executes wait_random n times.
-    '''
-    wait_times = await asyncio.gather(
-        *tuple(map(lambda _: wait_random(max_delay), range(n)))
-    )
-    return sorted(wait_times)
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """Asynchronously wait for n random time between 0 and max_delay
+    and return a sorted list of the delays
+
+    Args:
+        n (int): _description_
+        max_delay (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        List[float]: _description_
+    """
+    delay = []
+    for _ in range(n):
+        delay += [await wait_random(max_delay)]
+    return sorted(delay)
