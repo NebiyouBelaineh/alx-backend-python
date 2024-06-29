ec75e36b25ddbdc9ca98576ad3e2831d6090b196
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-06-29 22:49:17 +0300

    Add solution for task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,3 +21,14 @@ class TestGithubOrgClient(unittest.TestCase):
         # self.assertEqual(result, response)
         org_url = f"https://api.github.com/orgs/{org}"
         mock_org.assert_called_once_with(org_url)
+
+    def test_public_repos_url(self):
+        """Tests _public_repos_url method"""
+        with patch.object(client.GithubOrgClient,
+                          '_public_repos_url',
+                          new_callable=unittest.mock.PropertyMock
+                          ) as mock_a_property:
+            mocked_url = "https://api.github.com/mocked_url"
+            mock_a_property.return_value = mocked_url
+            instance = client.GithubOrgClient(org_name='google')
+            self.assertEqual(instance._public_repos_url, mocked_url)
